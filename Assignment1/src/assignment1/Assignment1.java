a60c782209d9ea6270bc668f0c90040d6b7045a9
Thanh Nguyen <thanhnguyencsbk@gmail.com>
2016-03-17 22:51:03 +0700

    First commit

---
+++ Assignment1/src/assignment1/Assignment1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ThanhNguyen
+ */
+public class Assignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    
+}
